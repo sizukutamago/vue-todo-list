d7c42763bb4d93fa347db916d44e8f380d29682d
sizukutamago <sizukutamago@gmail.com>
2018-01-27 23:02:08 +0900

    add input todo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 const vue = new Vue({
     el: '#app',
     data: {
-        items: [
-            {title: '店に行く'},
-            {title: '歯磨きする'},
-            {title: 'ゲームする'}
-        ]
+        items: [],
+        newTodoTitle: ''
+    },
+    methods: {
+        addTodo: function (title) {
+            this.items.push({
+                title: title,
+                isChecked: false
+            });
+            this.newTodoTitle = '';
+        }
     }
 })
