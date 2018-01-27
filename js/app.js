2445314e65812cb254b02ddeeacc03a585e34081
sizukutamago <sizukutamago@gmail.com>
2018-01-27 22:33:31 +0900

    add v-for list

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const vue = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            {title: '店に行く'},
+            {title: '歯磨きする'},
+            {title: 'ゲームする'}
+        ]
+    }
+})
